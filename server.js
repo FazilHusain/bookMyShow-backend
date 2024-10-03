ad0f8f400dda9e5bafc3f127a3998e97ca0b0e99
FazilHusain <fhmansoori99@gmail.com>
2024-10-03 15:51:02 +0530

    change it to PORT

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(express.urlencoded({ extended: false }))
 // creating an api and seperating it.
 app.use("/api", require("./route"));
 
-app.listen(8080,()=>{
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT,()=>{
     console.log("App listening to port 8080")
 });
