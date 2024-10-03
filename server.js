a4b375289f530dd02d07b444df33e793785b911c
FazilHusain <fhmansoori99@gmail.com>
2024-10-03 20:08:10 +0530

    changes

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.use(express.urlencoded({ extended: false }))
 // creating an api and seperating it.
 app.use("/api", require("./route"));
 
+const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3000'; // Temporary
+
+app.use(cors({
+  origin: frontendUrl,
+  credentials: true
+}));
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT,()=>{
